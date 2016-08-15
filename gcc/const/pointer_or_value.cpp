43ca7c40cefb792bfbcbc74492271c85fbadfb41
Hiroyuki Kurokawa <pxi05647@nifty.com>
2016-08-15 23:55:10 +0900

    Add argument variations.

--- gcc/const/pointer_or_value.cpp
+++ gcc/const/pointer_or_value.cpp
@@ -42,14 +42,34 @@ int strlen_c3(char* const str) // same as PSTR
 
 
 
+const char* C1 = "c1";
+char const* C2 = "c2";
+char* const C3 = "c3"; // warning: deprecated conversion from string constant to 'char*' [-Wwrite-strings]
 int main(int argc, char** argv)
 {
 	strlen_pstr("aaa"); // warning: deprecated conversion from string constant to 'PSTR {aka char*}' [-Wwrite-strings]
-	strlen_pcstr("aaa");
+//	strlen_pstr(C1); // error: invalid conversion from 'const char*' to 'PSTR {aka char*}' [-fpermissive]
+//	strlen_pstr(C2); // error: invalid conversion from 'const char*' to 'PSTR {aka char*}' [-fpermissive]
+	strlen_pstr(C3);
 
-	strlen_c1("aaa");   // warning: deprecated conversion from string constant to 'PSTR {aka char*}' [-Wwrite-strings]
+	
+	strlen_pcstr("aaa");
+	strlen_pcstr(C1);
+	strlen_pcstr(C2);
+	strlen_pcstr(C3);
+
+	strlen_c1("aaa");
+	strlen_c1(C1);
+	strlen_c1(C2);
+	strlen_c1(C3);
 	strlen_c2("aaa");
-	strlen_c3("aaa");   // warning: deprecated conversion from string constant to 'char*' [-Wwrite-strings]
+	strlen_c2(C1);
+	strlen_c2(C2);
+	strlen_c2(C3);
+	strlen_c3("aaa"); // warning: deprecated conversion from string constant to 'char*' [-Wwrite-strings]
+//	strlen_c3(C1);    // error: invalid conversion from 'const char*' to 'char*' [-fpermissive]
+//	strlen_c3(C2);    // error: invalid conversion from 'const char*' to 'char*' [-fpermissive]
+	strlen_c3(C3);
 	
 	return 0;
 }
