be245e436359dafcd8eae419bb6a23b070abd4b7
Hiroyuki Kurokawa <kurokawa@ubuntu-VirtualBox.(none)>
2017-09-06 01:55:37 +0900

    Add comment to explain result.

--- gcc/BINARY_HACK/29_not_export_symbol/build.sh
+++ gcc/BINARY_HACK/29_not_export_symbol/build.sh
@@ -2,6 +2,7 @@ gcc -fPIC -c a.c; gcc -fPIC -c b.c; gcc -shared -o libfoo.so a.o b.o
 nm -D libfoo.so | grep -v '_'
 # both of foo & bar are shown
 
+echo "---"
 gcc -fPIC -c a.c; gcc -fPIC -c b.c; gcc -shared -o libfoo.so a.o b.o -Wl,--version-script,libfoo.map
 nm -D libfoo.so | grep -v '_'
 # only foo is shown
--- gcc/BINARY_HACK/29_not_export_symbol/gcc_extension/build.sh
+++ gcc/BINARY_HACK/29_not_export_symbol/gcc_extension/build.sh
@@ -1,6 +1,8 @@
 g++ -o test.so -shared test.cc
 nm --demangle -D test.so | grep func
+# all symbols are shown
 
 echo "---"
-g++ -o test.so --fvisibility=hidden -shared test.cc
+g++ -o test.so -fvisibility=hidden -shared test.cc
 nm --demangle -D test.so | grep func
+# only EXPORT symbol is shown
