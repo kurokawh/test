7e187c4801e4ee61a5112f7454c7eeb4c38566ea
Hiroyuki Kurokawa <kurokawa@ubuntu-VirtualBox.(none)>
2017-09-10 16:22:08 +0900

    add required package information

--- gcc/BINARY_HACK/44_mudflap/build.sh
+++ gcc/BINARY_HACK/44_mudflap/build.sh
@@ -1,2 +1,9 @@
 gcc -g -fmudflap -o testflap testflap.c -lmudflap
 ./testflap
+
+# install libmudflap if following compile error occurs.
+#
+# cc1: fatal error: mf-runtime.h: No such file or directory
+# compilation terminated.
+#
+# % sudo apt-get install libmudflap0 libmudflap0-dbg libmudflap0-4.6-dev
