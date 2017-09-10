aafc6b9c31e8c8a4e4db30c2cdbca01a9defbec2
Hiroyuki Kurokawa <kurokawa@ubuntu-VirtualBox.(none)>
2017-09-10 13:54:37 +0900

    Fix compile error.

--- gcc/BINARY_HACK/30_versioned_symbol/build1.sh
+++ gcc/BINARY_HACK/30_versioned_symbol/build1.sh
@@ -1,5 +1,12 @@
 gcc -fPIC -c -o libmax1.o libmax1.c
-gcc -shared -Wl,-soname,libmax.so.1 -o libmax.so.1.0 libmax1.o
-ln -s libmax.so.1.0 libmax.so.1
-gcc -L. -lmax -o vertest1 vertest1.c
+# gcc -shared -Wl,-soname,libmax.so.1 -o libmax.so.1.0 libmax1.o
+# ln -s libmax.so.1.0 libmax.so.1
+
+#gcc -shared -Wl,-soname,libmax.so -o libmax.so.1.0 libmax1.o
+#ln -s libmax.so.1.0 libmax.so
+#gcc -L. -lmax -o vertest1 vertest1.c
+
+gcc -shared -o libmax.so libmax1.c
+gcc  vertest1.c -L. -lmax -o vertest1
+
 LD_LIBRARY_PATH=. ./vertest1
---
+++ gcc/BINARY_HACK/30_versioned_symbol/clean.sh
@@ -0,0 +1,4 @@
+rm *.o
+rm libmax.so*
+rm vertest1
+
