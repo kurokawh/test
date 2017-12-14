e214e9eda5642ddf286cbbcf1ea199a9613df3f7
Hiroyuki Kurokawa <hiroyuki.kurokawa@nifty.com>
2017-12-14 18:38:59 +0900

    Add file exstance check.

--- bat/if.bat
+++ bat/if.bat
@@ -15,7 +15,17 @@ if "%2"=="2" (
 
 if "%2" neq "x" (
 	echo "2nd or 3rd option is not `x`!"
-	shift /1
+REM	shift /1
 ) else (
 	echo "2nd or 3rd option is `x`!"
 )
+
+
+
+echo "check %2"
+if exist %2 (
+	echo "%2 exist!"
+) else (
+	echo "%2 is not file or directory."
+)
+
