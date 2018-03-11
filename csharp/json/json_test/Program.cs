e9fdc8d87778272a621c6e88754229ea23733d88
Hiroyuki Kurokawa <pxi05647@nifty.com>
2018-03-12 01:53:21 +0900

    csproj settings are copied from test project in Newtonsoft.

--- csharp/json/json_test/Program.cs
+++ csharp/json/json_test/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using Newtonsoft.Json.Linq;
 
 namespace json_test
 {
     class Program
     {
+        class Item
+        {
+            public int id = -1;
+            public string name = "no-name";
+        };
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int id = 111;
+            Item item = new Item();
+
+            JObject jobj = new JObject()
+                    {
+                        { "id", id },
+                        { "item", item },
+                    };
+            Console.WriteLine("jobj: " + jobj.ToString());
         }
     }
 }
