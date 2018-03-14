e780ab5be3d3b92c0115006524fa416968a4b145
Hiroyuki Kurokawa <pxi05647@nifty.com>
2018-03-15 00:43:29 +0900

    Use HOME environment variable to refer dll.

--- csharp/json/json_test/Program.cs
+++ csharp/json/json_test/Program.cs
@@ -7,6 +7,11 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
+// <<memo to build>>
+// create git repository under $HOME/tool directory and build dll in it.
+// % cd ~/tool
+// % git clone git@github.com:JamesNK/Newtonsoft.Json.git
+
 namespace json
 {
     class Program
