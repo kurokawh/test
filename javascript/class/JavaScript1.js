df7c6d48084bac896f9251b99310894dedd1fd9a
Hiroyuki Kurokawa <pxi05647@nifty.com>
2018-04-04 00:34:34 +0900

    add export class sample.

---
+++ javascript/class/JavaScript1.js
@@ -0,0 +1,41 @@
+﻿// https://html5experts.jp/takazudo/17355/
+
+/* Animalクラス */
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+    set name(name) {
+        this._name = name;
+    }
+    get name() {
+        return this._name;
+    }
+    walk() {
+        console.log(this.name + 'が歩いてます');
+    }
+}
+
+
+/* Catクラス */
+class Cat extends Animal {
+    constructor(name, color) {
+        super(name); // Animalクラスのconstructor呼び出し
+        this.color = color;
+    }
+    get color() {
+        return this._color;
+    }
+    set color(color) {
+        this._color = color;
+    }
+    mew() {
+        console.log(this.color + '色の' + this.name + 'がニャーと鳴いてます');
+    }
+    walk() {
+        super.walk(); // Animalクラスのwalkメソッド呼び出し
+        console.log('忍び足です');
+    }
+}
+
+module.exports = Cat;
--- javascript/class/app.js
+++ javascript/class/app.js
@@ -1,3 +1,20 @@
 'use strict';
 
+var Cat = require('./JavaScript1');
+
 console.log('Hello world');
+
+
+
+/* Catクラスのインスタンス作成 */
+var cat1 = new Cat('タマ', '茶');
+console.log(cat1.name); // タマ
+cat1.walk(); // タマが歩いてます
+// 忍び足です
+cat1.mew();  // 茶色のタマがニャーと鳴いてます
+
+var cat2 = new Cat('コタロー', '黒');
+console.log(cat2.name); // コタロー
+cat2.walk(); // コタローが歩いてます
+// 忍び足です
+cat2.mew();  // 黒色のコタローがニャーと鳴いてます
