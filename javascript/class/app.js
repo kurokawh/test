b5d56c5ee5960ff1055a73a32ca28fd8bdeae874
Hiroyuki Kurokawa <pxi05647@nifty.com>
2018-04-04 00:42:21 +0900

    add grand child class.

--- javascript/class/app.js
+++ javascript/class/app.js
@@ -18,3 +18,20 @@ console.log(cat2.name); // コタロー
 cat2.walk(); // コタローが歩いてます
 // 忍び足です
 cat2.mew();  // 黒色のコタローがニャーと鳴いてます
+
+
+class Lion extends Cat {
+    constructor(name, color) {
+        super('シンバ', 'ゴールド'); // Catクラスのconstructor呼び出し
+    }
+    run() {
+///        super.walk(); // Catクラスのwalkメソッド呼び出し
+        console.log('走る！');
+    }
+}
+
+var lion1 = new Lion();
+console.log(lion1.name);
+lion1.run();
+lion1.walk();
+lion1.mew();
