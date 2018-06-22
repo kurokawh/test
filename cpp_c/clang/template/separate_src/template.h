7b41f610c30c480d8f3a0bcd208d28bca225f706
Hiroyuki Kurokawa <hiroyuki.kurokawa@nifty.com>
2018-06-23 02:01:41 +0900

    add sample to avoid error: a typedef cannot be a template

--- cpp_c/clang/template/separate_src/template.h
+++ cpp_c/clang/template/separate_src/template.h
@@ -37,6 +37,16 @@ namespace test
 	};
 
 	typedef GenericContainer<int64_t> Container;
+
+//	template<typename ID>
+//	typedef std::vector<GenericContainer<ID>> GenericContainerList;
+//	=> error: a typedef cannot be a template
+//	https://cpprefjp.github.io/lang/cpp11/alias_templates.html
+//	http://www.yasutomo57jp.com/2014/06/12/typedef%E3%82%92%E3%83%86%E3%83%B3%E3%83%97%E3%83%AC%E3%83%BC%E3%83%88%E5%8C%96%E3%81%97%E3%81%9F%E3%81%84%EF%BC%88%E3%82%A8%E3%82%A4%E3%83%AA%E3%82%A2%E3%82%B9%E3%83%86%E3%83%B3%E3%83%97%E3%83%AC/
+	template<typename ID>
+	using GenericContainerList = std::vector<GenericContainer<ID>>;
+	typedef GenericContainerList<int64_t> Int64ContainerList;
+
 }
 
 #endif  //_TEMPLATE_H
