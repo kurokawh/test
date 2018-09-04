cca2f1f6b33bd2a6822362cba4caa3b36c24990c
Hiroyuki Kurokawa <kurokawa@rd.scei.sony.co.jp>
2018-09-04 22:16:45 +0900

    add arg sample.

--- bat/variable.bat
+++ bat/variable.bat
@@ -4,3 +4,8 @@ REM http://mmasashi.hatenablog.com/entry/20100207/1265557400
 
 for /f "usebackq tokens=*" %%i in (`cygpath -ua .`) do @set RESULT=%%i
 echo %RESULT%
+
+echo %1
+
+for /f "usebackq tokens=*" %%i in (`cygpath -ua %1 `) do @set RESULT1=%%i
+echo %RESULT1%
