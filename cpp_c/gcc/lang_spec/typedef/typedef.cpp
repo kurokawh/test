42ca4b81549d2814bd939490ad05a23b81bebf6f
Hiroyuki Kurokawa <kurokawa@rd.scei.sony.co.jp>
2018-09-06 08:31:13 +0900

    add re-define enum in another namespace sample

---
+++ cpp_c/gcc/lang_spec/typedef/typedef.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+namespace original {
+	enum OrgEnum {
+		ENUM_A,
+		ENUM_B,
+		ENUM_C,
+	};
+
+	void test(OrgEnum e)
+	{
+		return;
+	}
+}
+
+
+namespace mine {
+
+	// re-define Enum for my namespace
+	typedef original::OrgEnum MyEnum;
+
+	void test(MyEnum e)
+	{
+		return;
+	}
+
+};
+
+int main(int argc, char** argv)
+{
+	original::OrgEnum oe = original::ENUM_A;
+	mine::MyEnum me;
+
+	original::test(oe);
+	original::test(me);
+
+
+	me = original::ENUM_A; // XXX: "original" should not be used.
+	me = mine::MyEnum::ENUM_A; // OK!!!
+	mine::test(oe);
+	mine::test(me);
+
+	printf("OK!\n");
+	return 0;
+}
