6a09f0cab7fc74863465b36b529ae5b842f0d6f6
Hiroyuki Kurokawa <kurokawa@rd.scei.sony.co.jp>
2018-09-12 15:23:09 +0900

    fix error on python3. print needs parenthesis.

--- python/if/multiline_cond.py
+++ python/if/multiline_cond.py
@@ -5,15 +5,15 @@
 for i in range(1, 100):
     if i % 3 == 0  \
         or "2" in str(i):
-        print "2 or 3!: " + str(i)
+        print ("2 or 3!: " + str(i))
     else:
-        print str(i)
+        print (str(i))
 
 
 # ERROR if there is comment
 for i in range(1, 100):
     if i % 3 == 0  \ # 3
         or "2" in str(i): # or 2
-        print "2 or 3!: " + str(i)
+        print ("2 or 3!: " + str(i))
     else:
-        print str(i)
+        print (str(i))
