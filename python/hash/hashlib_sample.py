545905f39f64202ad4a6c7cff3ee9dbe5d16ba45
Hiroyuki Kurokawa <pxi05647@nifty.com>
2019-05-05 11:51:33 +0900

    show properties.

--- python/hash/hashlib_sample.py
+++ python/hash/hashlib_sample.py
@@ -10,6 +10,20 @@ f.close()
 
 hex_sha1 = hashlib.sha1(binary_data).hexdigest()
 print('SHA1 :', hex_sha1)
-
 hex_md5 = hashlib.md5(binary_data).hexdigest()
 print('MD5 :', hex_md5)
+
+# hash
+h = hashlib.sha1(binary_data);
+str_sha1 = h.digest()
+print('digest_size:', h.digest_size)
+print('block_size:', h.block_size)
+print('str-len:', len(str_sha1))
+
+
+# libhash
+print
+print('### properties in hashlib ###')
+print('hashlib.algorithms:', hashlib.algorithms)
+print('hashlib.algorithms_guaranteed:', hashlib.algorithms_guaranteed)
+print('hashlib.algorithms_available:', hashlib.algorithms_available)
