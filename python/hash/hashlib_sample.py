803fb932118da754f52b947c6a5c31da75c58472
Hiroyuki Kurokawa <pxi05647@nifty.com>
2019-05-05 11:05:33 +0900

    follw naming rule of PEP8.

--- python/hash/hashlib_sample.py
+++ python/hash/hashlib_sample.py
@@ -2,14 +2,14 @@
 
 import hashlib
 
-Path = 'hashlib_sample.py'
+path = 'hashlib_sample.py'
 
-f = open(Path,'rb')
-BinaryData = f.read()
+f = open(path,'rb')
+binary_data = f.read()
 f.close()
 
-SHA1 = hashlib.sha1(BinaryData).hexdigest()
+SHA1 = hashlib.sha1(binary_data).hexdigest()
 print('SHA1 :',SHA1)
 
-MD5 = hashlib.md5(BinaryData).hexdigest()
+MD5 = hashlib.md5(binary_data).hexdigest()
 print('MD5 :',MD5)
