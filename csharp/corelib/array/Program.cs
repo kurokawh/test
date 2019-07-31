dfb2af57e7c9391ca2317fb0d89dbcfcaa2013db
Hiroyuki Kurokawa <kurokawa@rd.scei.sony.co.jp>
2019-07-31 20:17:14 +0900

    add array Skip(), Take() sample.

--- csharp/corelib/array/Program.cs
+++ csharp/corelib/array/Program.cs
@@ -10,6 +10,13 @@ namespace array_test
 	{
 		static void Main(string[] args)
 		{
+			int[] ary1 = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+			int[] ary2 = ary1.Skip(2).Take(5).ToArray();
+			Console.WriteLine(ary2.Length);
+			int[] ary3 = ary1.Skip(8).Take(5).ToArray();
+			Console.WriteLine(ary3.Length);
+			int[] ary4 = ary1.Skip(20).Take(5).ToArray();
+			Console.WriteLine(ary4.Length);
 		}
 	}
 }
