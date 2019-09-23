3dafbb137cf9f892b98604f194f61a5f53ccfa98
Hiroyuki Kurokawa <pxi05647@nifty.com>
2019-09-23 21:20:35 +0900

    add string operation sample.

---
+++ python/string/string_operation.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+
+# https://www.headboost.jp/python-strings/
+# https://www.isc.meiji.ac.jp/~mizutani/python/intro3_python.html
+
+st = "abcdefabcghi.py"
+print(st[3:10])
+print(st[-2:])
+print(st[0:len(st) - 3])
+print("-------------------------")
+
+
+##
+text = '0123456789abcdefg'
+print(text[2])  #  idx: 2
+print(text[:6])  #  to idx: 6
+print(text[6:])  #  from idx: 6
+print(text[6:10])  #  idx from 6 to 9
