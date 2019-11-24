58164c1498ce89b722a45d7c28ffd179de516864
Hiroyuki Kurokawa <kurokawa@rd.scei.sony.co.jp>
2019-11-24 15:49:50 +0900

    add type decl sample.

--- cpp_c/gcc/stl/functional/functional.cpp
+++ cpp_c/gcc/stl/functional/functional.cpp
@@ -6,6 +6,8 @@ int add(int x) { return x + 1; }
 // https://leico.github.io/TechnicalNote/Cpp/lambda
 void test_caputer()
 {
+	std::cout << "test_capture()" << std::endl;
+
 	int i = 5;
 	int result = 0;
 
@@ -34,6 +36,31 @@ void test_caputer()
 
 }
 
+void type_decl()
+{
+	std::cout << "type_decl()" << std::endl;
+	int i = 0;
+	int result = 0;
+	std::function<int(int)> f = [](int x) { return x + 1;}; // OK
+
+	auto f2 = [i](int x) { return i + 1;}; // OK
+	std::function<int(int)> f3 = [i](int x) { return i + 1;}; // OK
+	result = f3(2);
+
+	std::cout << "result:"  << result  << std::endl;
+	std::cout << "i:" << i << std::endl;
+
+	{
+	auto fi = [&](int x) { i = 10; return i + 1;}; // OK
+	std::function<int(int)> fii = [&](int x) { i += 20; return i + 1;}; // OK
+	result = fi(2);
+	result = fii(2);
+	std::cout << "result:"  << result  << std::endl;
+	std::cout << "i:" << i << std::endl;
+	}
+}
+
+
 int main()
 {
   // 関数を代入
@@ -47,4 +74,5 @@ int main()
   std::cout << result << std::endl;
 
   test_caputer();
+  type_decl();
 }
