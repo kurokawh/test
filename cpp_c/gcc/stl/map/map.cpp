5f982a60d18f9944a3fd9569c0c8c89042f68620
Hiroyuki Kurokawa <kurokawa@rd.scei.sony.co.jp>
2020-01-27 22:31:17 +0900

    add key iterate sample.

--- cpp_c/gcc/stl/map/map.cpp
+++ cpp_c/gcc/stl/map/map.cpp
@@ -2,8 +2,25 @@
 #include <iostream>
 #include <map>
 
+int iterate_keys()
+{
+  std::map<char,int> mymap = {
+    {'a', 50},
+    {'b', 100},
+    {'c', 150},
+    {'d', 200},
+  };
+  int i = 0;
+  for (std::map<char,int>::iterator itr = mymap.begin(); itr != mymap.end(); itr++) {
+	  std::cout << i << ": " << itr->first << '\n';
+	  i++;
+  }
+  return 0;
+}
+
 int main ()
 {
+	iterate_keys();
 /*
   std::map<char,int> mymap;
   mymap['a']=50;
