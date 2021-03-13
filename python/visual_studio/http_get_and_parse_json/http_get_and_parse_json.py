9011864bd8416651f53490884ec0e4e060ac8031
Hiroyuki Kurokawa <pxi05647@nifty.com>
2021-03-13 21:21:06 +0900

    create function to get data.

--- python/visual_studio/http_get_and_parse_json/http_get_and_parse_json.py
+++ python/visual_studio/http_get_and_parse_json/http_get_and_parse_json.py
@@ -7,26 +7,30 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 
-title_id = "CUSA11456"
-  
-# api-endpoint 
-#URL = "http://maps.googleapis.com/maps/api/geocode/json"
-URL = "https://ps5.np.playstation.net/api/catalog/v2/titles/CUSA11456_00/concept"
-  
-  
-# sending get request and saving the response as response object 
-r = requests.get(url = URL, verify = False) 
-  
-# extracting data in json format 
-data = r.json() 
-  
-  
-# extracting latitude, longitude and formatted address  
-# of the first matching location 
-concept_id = data['id']
-name = data['nameEn']
-type = data['type']
 
-# printing the output 
-print("%s,%s,%s,%s"
-      %(title_id, concept_id, name, type)) 
+  
+
+def http_get(base, tid) :  
+    url = base + tid + "_00/concept"
+    print("url: " + url)
+
+    # sending get request and saving the response as response object 
+    r = requests.get(url, verify = False) 
+  
+    # extracting data in json format 
+    data = r.json() 
+  
+  
+    # extracting latitude, longitude and formatted address  
+    # of the first matching location 
+    concept_id = data['id']
+    name = data['nameEn']
+    type = data['type']
+
+    # printing the output 
+    print("%s,%s,%s,%s" %(title_id, concept_id, name, type)) 
+
+
+
+http_get("https://ps5.np.playstation.net/api/catalog/v2/titles/", "CUSA11456")
+
