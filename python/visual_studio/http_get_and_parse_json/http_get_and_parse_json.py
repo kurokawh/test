6a87b0777edbc689d27c195303ec3f2fc01b018d
Hiroyuki Kurokawa <pxi05647@nifty.com>
2021-03-13 23:26:52 +0900

    quote name.

--- python/visual_studio/http_get_and_parse_json/http_get_and_parse_json.py
+++ python/visual_studio/http_get_and_parse_json/http_get_and_parse_json.py
@@ -12,16 +12,15 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
   
 
 def http_get(base, tid) :  
-    url = base + tid + "_00/concept"
+    url = base + tid + "/concept"
     print("url: " + url)
 
     # sending get request and saving the response as response object 
     r = requests.get(url, verify = False) 
-  
+
     # extracting data in json format 
     data = r.json() 
-  
-  
+
     # extracting latitude, longitude and formatted address  
     # of the first matching location 
     concept_id = data['id']
@@ -29,15 +28,14 @@ def http_get(base, tid) :
     type = data['type']
 
     # printing the output 
-    print("%s,%s,%s,%s" %(tid, concept_id, name, type)) 
+    print("%s,\"%s\",%s,%s," %(concept_id, name, type, tid)) 
 
 
 # xxx https://ps5.np.playstation.net/api/catalog/v2/titles/ CUSA11456
 #http_get(sys.argv[1], sys.argv[2])
 
-
 # for i in range (0, 10):
-for i in range (11456, 11460):
-    tid = "CUSA" + str(i).zfill(5)
+for i in range (11456, 11470):
+    tid = "CUSA" + str(i).zfill(5) + "_00"
     http_get("https://ps5.np.playstation.net/api/catalog/v2/titles/", tid)
 
