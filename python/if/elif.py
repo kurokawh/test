7cc910db28513b90f8ae4d1dde14c71996f8d908
Hiroyuki Kurokawa <kurokawa@rd.scei.sony.co.jp>
2021-11-13 00:10:26 +0900

    add elif sample.

---
+++ python/if/elif.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+string = input("文字列を入力してください:")
+if string.isdecimal():
+    print(string, "は数字です")
+elif string.isalpha():
+    print(string, "はアルファベットです")
+else:
+    print(string, "は数字でもアルファベットでもありません")
