91031478d32c02c41b4ab82350e7d8ce18d6573c
Hiroyuki Kurokawa <kurokawa@rd.scei.sony.co.jp>
2022-03-30 22:29:31 +0900

    newly generated.

--- csharp/memory/Program.cs
+++ csharp/memory/Program.cs
@@ -2,11 +2,11 @@
 
 namespace ConsoleApp
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
 }
