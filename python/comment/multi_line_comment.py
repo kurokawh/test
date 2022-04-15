f0223d7e60c79b67c01e6e6fb9eb43f9ca9e04e2
Hiroyuki Kurokawa <kurokawa@rd.scei.sony.co.jp>
2022-04-15 19:36:37 +0900

    add multi line comment sample.

---
+++ python/comment/multi_line_comment.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+this is a sample
+of  multi line comment.
+works fine!
+"""
+
+'''
+another multiple
+line comment
+'''
+
+print ("""this is
+multi line
+string.
+""")
