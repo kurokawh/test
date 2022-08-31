911298d73a0497ecb10df82c61ac653c36e913f4
Hiroyuki Kurokawa <kurokawa@rd.scei.sony.co.jp>
2022-09-01 00:50:13 +0900

    add comment that explains  c++ spec.

--- cpp_c/gcc/lang_spec/main/main.cpp
+++ cpp_c/gcc/lang_spec/main/main.cpp
@@ -1,4 +1,13 @@
 // main spec test
+// 
+// spec says: https://en.cppreference.com/w/cpp/language/main_function
+//   The main function has several special properties:
+//   1) It cannot be used anywhere in the program
+//     a) in particular, it cannot be called recursively
+//     b) its address cannot be taken
+//
+// but g++ and clang allows these a) & b) behavior.
+
 #include <stdio.h>
 #include <functional>
 
@@ -13,8 +22,8 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
-//	int ret = main(argc - 1, argv);
-	int ret = fp2(argc - 1, argv); // recursive call to main
+	int ret = main(argc - 1, argv);
+//	int ret = fp2(argc - 1, argv); // recursive call to main
 	if (ret < 0) {
 		printf(" main: ret: %d\n", ret);
 		return ret;
