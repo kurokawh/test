95e34cfd24b34828dcf767e6d1a3a01bbf66e591
Hiroyuki Kurokawa <kurokawa@rd.scei.sony.co.jp>
2023-09-12 14:45:07 +0900

    add MIN/MAX sample which show specific row.

--- sql/group_by/which_row_is_shown.sql
+++ sql/group_by/which_row_is_shown.sql
@@ -35,3 +35,27 @@ SELECT * FROM tbl ORDER BY str DESC
 ) 
 GROUP BY dup;
 --> first row for each distinction is shown
+
+
+
+--SELECT dup, str, MIN(num) FROM 
+SELECT dup, str, MAX(num) FROM 
+(
+WITH tbl(dup, str, num) AS
+     (SELECT *
+     	     FROM (VALUES (1, 'a', 100),
+             	  	  (1, 'b', 101),
+             	  	  (1, 'a', 102),
+             	  	  (1, 'b',  99),
+             	  	  (1, 'b', 104),
+			  (2, 'a', 105),
+             	  	  (2, 'b', 106),
+             	  	  (2, 'a',  98),
+             	  	  (2, 'b', 108),
+             	  	  (2, 'c', 109),
+             	  	  (1, 'c', 110),
+			  (NULL, NULL, NULL)))
+SELECT * FROM tbl ORDER BY str DESC
+) 
+GROUP BY dup;
+-- if MAX/MIN is set, correct row which has MAX/MIN num is returned.
